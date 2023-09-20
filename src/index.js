1fc3e879a4e71dca8d3f1a33d9b84918c14f1ddb
luckfeat <luckfeat@gmail.com>
2023-09-20 19:02:22 +0900

    refactor: Update else if to switch at index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import todoTemplate from './template/todo.template';
 
 const URL = 'http://localhost:3000/todos';
+const todoTag = get('.todos');
+const pagination = get('.pagination');
+const form = get('.todo_form');
+const formInput = get('.todo_input');
 
-const get = (target) => {
+let currentPage = 1;
+let maxPage;
+
+function get(target) {
   return document.querySelector(target);
-};
-
-let todoTag = get('.todos');
-let form = get('.todo_form');
-let formInput = get('.todo_input');
+}
 
 function createToDoElement(item) {
   const { id, content, completed } = item;
@@ -19,111 +22,191 @@ function createToDoElement(item) {
   return todoItem;
 }
 
-async function getTodo() {
+async function renderTodo() {
+  const response = await fetch(URL);
+  const todos = await response.json();
+  const todoLength = todos.length;
+  const todoLimit = 10;
+  const totalPage = Math.ceil(todoLength / todoLimit);
+
+  function render() {
+    todoTag.innerHTML = '';
+    for (
+      let i = currentPage * todoLimit - todoLimit;
+      i < (currentPage === totalPage ? todoLength : currentPage * todoLimit);
+      i++
+    ) {
+      if (!todos[i]) {
+        alert('일이 없네요...');
+        break;
+      } else {
+        todoTag.appendChild(createToDoElement(todos[i]));
+      }
+    }
+  }
+
+  render();
+}
+
+async function renderPagination() {
   try {
     const response = await fetch(URL);
     const todos = await response.json();
-    todos.forEach((todo) => {
-      todoTag.appendChild(createToDoElement(todo));
-    });
-  } catch {
-    console.error('목록이 비어있습니다.');
+    const todoLength = todos.length;
+    const todoLimit = 10;
+    const totalPage = Math.ceil(todoLength / todoLimit);
+    maxPage = totalPage;
+
+    console.log(maxPage);
+
+    pagination.innerHTML = '';
+
+    for (let i = 0; i < totalPage; i++) {
+      const paginationButton = document.createElement('button');
+      paginationButton.className = 'pageNumber';
+      paginationButton.id = `page_${i + 1}`;
+      paginationButton.textContent = i + 1;
+      pagination.append(paginationButton);
+      paginationButton.addEventListener('click', (e) => {
+        currentPage = Number(e.target.innerText);
+        renderAll();
+      });
+    }
+    const currentPageNumber = get(`.pageNumber#page_${currentPage}`);
+    if (currentPageNumber) {
+      currentPageNumber.style.color = '#9dc0e8';
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const todo = {
-    content: formInput.value,
-    completed: false,
-  };
-  const postOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(todo),
-  };
-  fetch(URL, postOptions)
+function renderAll() {
+  return Promise.resolve()
     .then(() => {
-      todoTag.innerHTML = '';
-      getTodo();
+      renderTodo();
     })
     .then(() => {
-      formInput.value = '';
-      formInput.focus();
+      renderPagination();
     });
-});
+}
 
-todoTag.addEventListener('click', (e) => {
-  const item = e.target.closest('.item');
-  const label = item.querySelector('label');
-  const editInput = item.querySelector('input[type="text"]');
-  const contentButtons = item.querySelector('.content_buttons');
-  const editButtons = item.querySelector('.edit_buttons');
-  const value = editInput.value;
-  const content = value;
-  const id = item.dataset.id;
-  const completed = e.target.checked;
-  if (e.target.className === 'todo_checkbox') {
-    fetch(`${URL}/${id}`, {
-      method: 'PATCH',
+function createTodo() {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const todo = {
+      content: formInput.value,
+      completed: false,
+    };
+    const postOptions = {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ completed }),
-    })
+      body: JSON.stringify(todo),
+    };
+    if (todo.content === '') {
+      alert('일하기 싫은 자 먹지도 말라');
+      return;
+    }
+    fetch(URL, postOptions)
       .then(() => {
         todoTag.innerHTML = '';
-        getTodo();
+        return renderAll();
       })
-      .catch((error) => {
-        console.error(error);
-      });
-  } else if (e.target.className === 'todo_edit_button') {
-    label.style.display = 'none';
-    editInput.style.display = 'block';
-    contentButtons.style.display = 'none';
-    editButtons.style.display = 'block';
-    editInput.focus();
-    editInput.value = '';
-    editInput.value = value;
-  } else if (e.target.className === 'todo_edit_cancel_button') {
-    label.style.display = 'block';
-    editInput.style.display = 'none';
-    contentButtons.style.display = 'block';
-    editButtons.style.display = 'none';
-    editInput.value = label.innerText;
-  } else if (e.target.className === 'todo_edit_confirm_button') {
-    fetch(`${URL}/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ content }),
-    })
       .then(() => {
-        todoTag.innerHTML = '';
-        getTodo();
+        currentPage = maxPage;
+        console.log(currentPage);
+        formInput.value = '';
+        formInput.focus();
       })
       .catch((error) => {
-        console.error(error);
+        alert(error);
       });
-  } else if (e.target.className === 'todo_remove_button') {
-    fetch(`${URL}/${id}`, {
-      method: 'DELETE',
-    })
-      .then(() => {
-        todoTag.innerHTML = '';
-        getTodo();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-  return;
-});
+  });
+}
+
+function addTodoEvent() {
+  todoTag.addEventListener('click', (e) => {
+    const item = e.target.closest('.item');
+    const label = item?.querySelector('label');
+    const editInput = item?.querySelector('input[type="text"]');
+    const contentButtons = item?.querySelector('.content_buttons');
+    const editButtons = item?.querySelector('.edit_buttons');
+    const value = editInput?.value;
+    const content = value;
+    const id = item?.dataset.id;
+    const completed = e.target.checked;
+
+    switch (e.target.className) {
+      case 'todo_checkbox':
+        fetch(`${URL}/${id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ completed }),
+        })
+          .then(() => {
+            todoTag.innerHTML = '';
+            getTodo();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        break;
+      case 'todo_edit_button':
+        label.style.display = 'none';
+        editInput.style.display = 'block';
+        contentButtons.style.display = 'none';
+        editButtons.style.display = 'block';
+        editInput.focus();
+        editInput.value = '';
+        editInput.value = value;
+        break;
+      case 'todo_edit_cancel_button':
+        label.style.display = 'block';
+        editInput.style.display = 'none';
+        contentButtons.style.display = 'block';
+        editButtons.style.display = 'none';
+        editInput.value = label.innerText;
+        break;
+      case 'todo_edit_cancel_button':
+        fetch(`${URL}/${id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ content }),
+        })
+          .then(() => {
+            todoTag.innerHTML = '';
+            renderAll();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        break;
+      case 'todo_remove_button':
+        fetch(`${URL}/${id}`, {
+          method: 'DELETE',
+        })
+          .then(() => {
+            todoTag.innerHTML = '';
+            renderAll();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+        break;
+    }
+
+    return;
+  });
+}
 
 window.addEventListener('DOMContentLoaded', () => {
-  getTodo();
+  renderAll();
+  createTodo();
+  addTodoEvent();
 });
